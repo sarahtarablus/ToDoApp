29ecb6ce59b60016b82e3857925713ffeda7149d
sarahTarablus <sarahtarablus@gmail.com>
2020-12-19 20:48:38 -0800

    this is my first commit

--- app.js
+++ app.js
@@ -1,1 +1,85 @@
+const input = document.getElementById('new-task');
+const addBtn = document.getElementById('addTask-btn');
+const list = document.querySelector('.list');
+const clearBtn =  document.getElementById('clear-btn');
+
+
+
+addBtn.addEventListener('click', addNewTask);
+list.addEventListener('click', removeTask);
+list.addEventListener('click', crossTask);
+clearBtn.addEventListener('click', clearTasks);
+
+function addNewTask(e){
+  if(input.value !== ''){
+    const li = document.createElement('li');
+    li.className = 'list-item';
+    li.appendChild(document.createTextNode(input.value));
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.className = 'check-box';
+    li.appendChild(checkBox);
+    const x = document.createElement('a');
+    x.className = 'delete-btn';
+    x.innerHTML = 'X'
+    x.style.backgroundColor = 'rgb(255, 204, 94)';
+    x.style.color = 'white';
+    x.style.cursor = 'pointer';
+    li.appendChild(x);
+    list.appendChild(li);
+
+    clearBtn.style.display = 'block';
+  } else {
+    alert('Make sure to add a task!');
+  }
+  addToLocalStorage(input.value);
+  input.value = '';
+e.preventDefault();
+}
+
+/*function alertMessage(msg){
+  const div = document.createElement('div');
+  div.className = 'message';
+  div.appendChild(document.createTextNode(msg));
+
+  const container = document.querySelector('.container-1');
+  const container2 = document.querySelector('.container-2');
+  container2.insertBefore(div, container);
+}*/
+
+function removeTask(e){
+  if(e.target.classList.contains('delete-btn')){
+    e.target.parentElement.remove();
+  }
+}
+
+function crossTask(e){
+  if(e.target.classList.contains('check-box')){
+    const li = e.target.parentElement;
+    li.style.textDecoration = 'line-through';
+    /*e.target.siblingChild.contains('delete-btn')
+      const x = e.target.siblingChild;
+      x.style.textDecoration = 'none';*/
+    }
+  }
+
+function clearTasks(){
+  while(list.firstChild){
+    list.removeChild(list.firstChild);
+    clearBtn.style.display = 'none';
+  }
+}
+
+function addToLocalStorage(task){
+  let tasks = [];
+  if(localStorage.getItem('tasks') === ''){
+    const li = document.createElement('li');
+    li.className = 'list-item';
+    li.appendChild(document.createTextNode(task));
+    tasks.push(task);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+  }
+}
+ 
+
 
